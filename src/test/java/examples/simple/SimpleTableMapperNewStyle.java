a2092ea316745532279245922b1a6f321923dce1
Jeff Butler <jeffgbutler@gmail.com>
2019-08-09 14:53:13 -0400

    Move the new style example to the main package

--- src/test/java/examples/simple/SimpleTableMapperNewStyle.java
+++ src/test/java/examples/simple/SimpleTableMapperNewStyle.java
@@ -13,7 +13,7 @@
  *    See the License for the specific language governing permissions and
  *    limitations under the License.
  */
-package examples.simple.newstyle;
+package examples.simple;
 
 import static examples.simple.legacy.SimpleTableDynamicSqlSupport.*;
 import static org.mybatis.dynamic.sql.SqlBuilder.*;
