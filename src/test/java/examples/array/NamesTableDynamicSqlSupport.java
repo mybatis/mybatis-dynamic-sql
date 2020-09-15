721837336d9542e84f0a7b62e6f10be0fffc9902
Jeff Butler <jeffgbutler@gmail.com>
2020-09-15 13:48:04 -0400

    Add tests for JDBC Arrays

---
+++ src/test/java/examples/array/NamesTableDynamicSqlSupport.java
@@ -0,0 +1,35 @@
+/*
+ *    Copyright 2016-2020 the original author or authors.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+package examples.array;
+
+import org.mybatis.dynamic.sql.SqlColumn;
+import org.mybatis.dynamic.sql.SqlTable;
+
+import java.sql.JDBCType;
+
+public class NamesTableDynamicSqlSupport {
+    public static final NamesTable namesTable = new NamesTable();
+    public static final SqlColumn<Integer> id = namesTable.id;
+    public static final SqlColumn<String[]> names = namesTable.names;
+
+    public static final class NamesTable extends SqlTable {
+        public NamesTable() {
+            super("NamesTable");
+        }
+        public final SqlColumn<Integer> id = column("id", JDBCType.INTEGER);
+        public final SqlColumn<String[]> names = column("names", JDBCType.ARRAY);
+    }
+}
---
+++ src/test/java/examples/array/NamesTableMapper.java
@@ -0,0 +1,44 @@
+/*
+ *    Copyright 2016-2020 the original author or authors.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+package examples.array;
+
+import org.apache.ibatis.annotations.InsertProvider;
+import org.apache.ibatis.annotations.Result;
+import org.apache.ibatis.annotations.ResultMap;
+import org.apache.ibatis.annotations.Results;
+import org.apache.ibatis.annotations.SelectProvider;
+import org.mybatis.dynamic.sql.insert.render.GeneralInsertStatementProvider;
+import org.mybatis.dynamic.sql.select.render.SelectStatementProvider;
+import org.mybatis.dynamic.sql.util.SqlProviderAdapter;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface NamesTableMapper {
+    @SelectProvider(type=SqlProviderAdapter.class, method="select")
+    @Results(id="NamesTableResult", value={
+            @Result(column="id", property="id", id=true),
+            @Result(column="names", property="names", typeHandler = StringArrayTypeHandler.class)
+    })
+    List<NamesRecord> selectMany(SelectStatementProvider selectStatement);
+
+    @SelectProvider(type=SqlProviderAdapter.class, method="select")
+    @ResultMap("NamesTableResult")
+    Optional<NamesRecord> selectOne(SelectStatementProvider selectStatement);
+
+    @InsertProvider(type= SqlProviderAdapter.class, method="generalInsert")
+    int generalInsert(GeneralInsertStatementProvider insertStatement);
+}
