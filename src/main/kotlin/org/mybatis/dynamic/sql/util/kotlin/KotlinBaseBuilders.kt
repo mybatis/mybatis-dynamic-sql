6665235d3e9b08b5e99c4301002bc69046d7265d
Jeff Butler <jeffgbutler@gmail.com>
2021-03-19 16:52:26 -0400

    Add composition functions for WhereApplier

--- src/main/kotlin/org/mybatis/dynamic/sql/util/kotlin/KotlinBaseBuilders.kt
+++ src/main/kotlin/org/mybatis/dynamic/sql/util/kotlin/KotlinBaseBuilders.kt
@@ -1,5 +1,5 @@
 /*
- *    Copyright 2016-2020 the original author or authors.
+ *    Copyright 2016-2021 the original author or authors.
  *
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
@@ -29,6 +29,11 @@ import org.mybatis.dynamic.sql.where.AbstractWhereSupport
 annotation class MyBatisDslMarker
 
 typealias WhereApplier = AbstractWhereDSL<*>.() -> Unit
+
+fun WhereApplier.andThen(after: WhereApplier): WhereApplier = {
+    invoke(this)
+    after(this)
+}
 
 @MyBatisDslMarker
 @Suppress("TooManyFunctions")
