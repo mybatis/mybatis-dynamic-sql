178f5ac6817f98a6136b23cca0daa1bfccba46bd
Jeff Butler <jeffgbutler@gmail.com>
2022-06-13 11:52:15 -0400

    Refactor the compiler tests, write some failing tests for the between methods

--- src/test/kotlin/nullability/test/NullabilityTest.kt
+++ src/test/kotlin/nullability/test/NullabilityTest.kt
@@ -1,6 +1,7 @@
 package nullability.test
 
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 
 class NullabilityTest {
@@ -21,7 +22,80 @@ class NullabilityTest {
             }
         """.trimIndent().lines()
 
-        val matchingErrors = compileIt(sourceLines, listOf(ExpectedErrorLocation(10,32)))
-        assertThat(matchingErrors).isEqualTo(1)
+        val compilerErrorReports = compileIt(sourceLines)
+        assertThat(compilerErrorReports).hasSize(1)
+        val matchCount = compilerErrorReports.matchCount(ExpectedErrorLocation(10,32))
+        assertThat(matchCount).isEqualTo(1)
    }
+
+    @Test
+    fun `Test That First Null In Between Method Causes Compile Error`() {
+        val sourceLines = """
+            package temp.kotlin.test
+            
+            import examples.kotlin.mybatis3.canonical.PersonDynamicSqlSupport.id
+            import examples.kotlin.mybatis3.canonical.PersonDynamicSqlSupport.person
+            import org.mybatis.dynamic.sql.util.kotlin.mybatis3.countFrom
+
+            fun testFunction() {
+                countFrom(person) {
+                    where { id isBetween null and 4 }
+                }
+            }
+        """.trimIndent().lines()
+
+        val compilerErrorReports = compileIt(sourceLines)
+        assertThat(compilerErrorReports).hasSize(1)
+        val matchCount = compilerErrorReports.matchCount(ExpectedErrorLocation(9,30))
+        assertThat(matchCount).isEqualTo(1)
+    }
+
+    @Disabled
+    @Test
+    fun `Test That Second Null In Between Method Causes Compile Error`() {
+        val sourceLines = """
+            package temp.kotlin.test
+            
+            import examples.kotlin.mybatis3.canonical.PersonDynamicSqlSupport.id
+            import examples.kotlin.mybatis3.canonical.PersonDynamicSqlSupport.person
+            import org.mybatis.dynamic.sql.util.kotlin.mybatis3.countFrom
+
+            fun testFunction() {
+                countFrom(person) {
+                    where { id isBetween 4 and null }
+                }
+            }
+        """.trimIndent().lines()
+
+        val compilerErrorReports = compileIt(sourceLines)
+        assertThat(compilerErrorReports).hasSize(1)
+        val matchCount = compilerErrorReports.matchCount(ExpectedErrorLocation(9,36))
+        assertThat(matchCount).isEqualTo(1)
+    }
+
+    @Disabled
+    @Test
+    fun `Test That Both Null In Between Method Causes Compile Error`() {
+        val sourceLines = """
+            package temp.kotlin.test
+            
+            import examples.kotlin.mybatis3.canonical.PersonDynamicSqlSupport.id
+            import examples.kotlin.mybatis3.canonical.PersonDynamicSqlSupport.person
+            import org.mybatis.dynamic.sql.util.kotlin.mybatis3.countFrom
+
+            fun testFunction() {
+                countFrom(person) {
+                    where { id isBetween null and null }
+                }
+            }
+        """.trimIndent().lines()
+
+        val compilerErrorReports = compileIt(sourceLines)
+        assertThat(compilerErrorReports).hasSize(2)
+        val matchCount = compilerErrorReports.matchCount(
+            ExpectedErrorLocation(9,30),
+            ExpectedErrorLocation(9, 39)
+        )
+        assertThat(matchCount).isEqualTo(2)
+    }
 }
