f67bc5dab3b9b1ee167c60710cdcb6c9b5f7bcd8
Jeff Butler <jeffgbutler@gmail.com>
2022-08-25 11:03:49 -0400

    Deprecate the callback interface in favor of configuration functions

--- src/main/java/org/mybatis/dynamic/sql/Callback.java
+++ src/main/java/org/mybatis/dynamic/sql/Callback.java
@@ -1,5 +1,5 @@
 /*
- *    Copyright 2016-2020 the original author or authors.
+ *    Copyright 2016-2022 the original author or authors.
  *
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
@@ -17,6 +17,10 @@ package org.mybatis.dynamic.sql;
 
 import java.util.function.Function;
 
+/**
+ * @deprecated in favor of the new statement configuration functionality
+ */
+@Deprecated
 @FunctionalInterface
 public interface Callback {
     void call();
