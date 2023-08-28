03c14160b91795a16c691634c7c06fba4a667867
Jeff Butler <jeffgbutler@gmail.com>
2023-08-28 16:11:40 -0400

    Remove unused imports

--- src/test/java/org/mybatis/dynamic/sql/InvalidSQLTest.java
+++ src/test/java/org/mybatis/dynamic/sql/InvalidSQLTest.java
@@ -27,7 +27,6 @@ import java.util.Collections;
 import java.util.List;
 import java.util.MissingResourceException;
 import java.util.Optional;
-import java.util.concurrent.atomic.AtomicInteger;
 
 import org.junit.jupiter.api.Test;
 import org.mybatis.dynamic.sql.exception.DynamicSqlException;
