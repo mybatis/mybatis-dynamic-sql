bd278964c48c1d30cc968e4dffa46737c11cca62
Jeff Butler <jeffgbutler@gmail.com>
2023-08-31 10:49:55 -0400

    Better Exception

--- src/main/java/org/mybatis/dynamic/sql/select/aggregate/Sum.java
+++ src/main/java/org/mybatis/dynamic/sql/select/aggregate/Sum.java
@@ -17,7 +17,7 @@ package org.mybatis.dynamic.sql.select.aggregate;
 
 import org.mybatis.dynamic.sql.BindableColumn;
 import org.mybatis.dynamic.sql.VisitableCondition;
-import org.mybatis.dynamic.sql.exception.DynamicSqlException;
+import org.mybatis.dynamic.sql.exception.InvalidSqlException;
 import org.mybatis.dynamic.sql.render.RenderingContext;
 import org.mybatis.dynamic.sql.select.function.AbstractUniTypeFunction;
 import org.mybatis.dynamic.sql.util.FragmentAndParameters;
@@ -55,7 +55,7 @@ public class Sum<T> extends AbstractUniTypeFunction<T, Sum<T>> {
 
     private FragmentAndParameters renderWithCondition(RenderingContext renderingContext) {
         if (!condition.shouldRender()) {
-            throw new DynamicSqlException(Messages.getString("ERROR.37", "sum")); //$NON-NLS-1$ //$NON-NLS-2$
+            throw new InvalidSqlException(Messages.getString("ERROR.37", "sum")); //$NON-NLS-1$ //$NON-NLS-2$
         }
 
         DefaultConditionVisitor<T> visitor = new DefaultConditionVisitor.Builder<T>()
